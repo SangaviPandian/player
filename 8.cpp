f146b2ba37c393564a597e6d375f799969ae685a
SangaviPandian <36364726+SangaviPandian@users.noreply.github.com>
2018-03-24 23:58:20 -0700

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include<string.h>
+int main() 
+{
+	char t[10];  int i;
+    gets(c);
+    c[0]=c[0]-32;
+    for(i=0;i<10;i++)
+    {
+      if(c[i]==' ')
+      {
+          c[i+1]=c[i+1]-32;
+      }
+    }
+    cout<<c;
+	
+	return 0;
+}
+  
+  
+  
